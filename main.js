dcb5f463aad67e6c9c6c5374fc73295705d7ecf1
Preetham Salehundam <preetham.salehundam@ge.com>
2018-06-19 22:01:17 +0530

    changes

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 var path = require('path');
 var fs = require('fs');
+// module.exports={
 
+// }
 var content = fs.readFileSync(path.join(__dirname,'baseline',"feature_templates","Auth-token.feature.template"));
 var csv_content = fs.readFileSync(path.join(__dirname,'baseline',"features","data.csv"))
+csv_content.replace()
 content=content.toString().replace("{{examples}}",csv_content.toString())
 var feature_writer = fs.createWriteStream(path.join(__dirname,'baseline',"features","Auth-token.feature"))
 feature_writer.write(content);
